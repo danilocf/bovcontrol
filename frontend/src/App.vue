efbb14e8b2531026c572ed9edcdbcb410a49b8a5
Danilo Freire <danilo.cf.sjc@gmail.com>
2020-04-10 17:12:42 -0300

    frontend wip farm card

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <!-- FIXME: -->
-    <img src="@/assets/logo.svg" style="position: fixed; top: 0; left: 10px" />
+    <img src="@/assets/logo.svg" style="position: fixed; top: 0; left: 14px" />
 
     <v-bottom-navigation v-model="bottomNav" app dark shift mandatory>
       <v-btn value="View">
@@ -17,16 +17,74 @@
     <v-content>
       <v-container fluid>
         <v-row>
-          <v-col cols="6"> </v-col>
-          <v-col cols="6" class="pa-5">
-            <v-card class="mb-4">
-              <v-card-title>Farm 1</v-card-title>
-              <v-card-text>Text</v-card-text>
-            </v-card>
+          <v-col cols="5"> </v-col>
+          <v-col cols="7">
+            <div class="d-flex flex-wrap">
+              <v-card
+                v-for="(farm, index) in farms"
+                :key="index"
+                outlined
+                class="font-weight-medium mb-4 mx-2"
+                width="250px"
+                @click="sheet = true"
+              >
+                <v-img src="@/assets/farm.jpg" />
+                <v-card-text class="pb-2">
+                  <h3 class="black--text mb-1">
+                    {{ farm.name }}
+                  </h3>
+                  <p class="mb-2">
+                    <small class="font-weight-light text--secondary"
+                      >Owner</small
+                    >
+                    {{ farm.owner }}
+                  </p>
+                  <!-- FIXME: -->
+                  <small
+                    class="d-inline-flex align-center text--secondary mb-0"
+                    style="font-size: .8rem"
+                  >
+                    <v-icon small>room</v-icon>
+                    +{{ farm.lat }} -{{ farm.long }}
+                  </small>
+                </v-card-text>
+              </v-card>
+            </div>
           </v-col>
         </v-row>
       </v-container>
     </v-content>
+
+    <v-bottom-sheet v-model="sheet">
+      <v-sheet class="text-center" height="200px">
+        <v-btn class="mt-6" text color="red" @click="sheet = !sheet"
+          >close</v-btn
+        >
+        <!-- FIXME: -->
+        <div class="py-3">
+          <p class="mb-0">
+            <small class="font-weight-light text--secondary">Address</small>
+            {{ farms[0].address }}
+          </p>
+          <p class="mb-0">
+            <small
+              class="d-inline-flex align-center font-weight-light mr-1 mb-0"
+              style="font-size: .8rem"
+            >
+              <v-icon small class="mr-1">schedule</v-icon>
+              {{ farms[0].createdAt }}
+            </small>
+            <small
+              class="d-inline-flex align-center font-weight-light mr-1 mb-0"
+              style="font-size: .8rem"
+            >
+              <v-icon small class="mr-1">history</v-icon>
+              {{ farms[0].updatedAt }}
+            </small>
+          </p>
+        </div>
+      </v-sheet>
+    </v-bottom-sheet>
 
     <v-fab-transition>
       <v-btn
@@ -64,7 +122,49 @@ export default {
   name: "App",
   data: () => ({
     bottomNav: "view",
-    dialog: false
-  })
+    sheet: false,
+    dialog: false,
+    farms: [
+      {
+        name: "Freire's Farm",
+        owner: "Danilo Freire",
+        address:
+          "Rua Doutor Tertuliano Delphim Júnior, São José dos Campos - SP",
+        lat: "40.689060",
+        long: "74.044636",
+        createdAt: "04-10-2020 3:56 PM",
+        updatedAt: "04-10-2020 3:56 PM",
+        showTime: false
+      },
+      {
+        name: "Freire's Farm",
+        owner: "Danilo Freire",
+        address:
+          "Rua Doutor Tertuliano Delphim Júnior, São José dos Campos - SP",
+        lat: "40.689060",
+        long: "74.044636",
+        createdAt: "04-10-2020 3:56 PM",
+        updatedAt: "04-10-2020 3:56 PM",
+        showTime: false
+      },
+      {
+        name: "Freire's Farm",
+        owner: "Danilo Freire",
+        address:
+          "Rua Doutor Tertuliano Delphim Júnior, São José dos Campos - SP",
+        lat: "40.689060",
+        long: "74.044636",
+        createdAt: "04-10-2020 3:56 PM",
+        updatedAt: "04-10-2020 3:56 PM",
+        showTime: false
+      }
+    ]
+  }),
+  mounted() {
+    // setInterval(() => {
+    //   this.farms.push(Object.assign({}, this.farms[0]))
+    //   this.$vuetify.goTo(9999)
+    // }, 1000)
+  }
 };
 </script>
